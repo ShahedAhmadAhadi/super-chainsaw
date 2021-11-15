6854543abaf7b07e63652c2d6709ad4e2113dee4
Shahed Ahmad <shaheda.a16@gmail.com>
2021-11-15 21:47:15 +0430

    react-query added to project and new client created for the whole app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import ComponentA from './Components/ComponentA';
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient();
 
 function App() {
+  
   return (
+    <QueryClientProvider client={queryClient}>
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -21,6 +27,7 @@ function App() {
         <ComponentA />
       </header>
     </div>
+    </QueryClientProvider>
   );
 }
 
