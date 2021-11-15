43f71e75f74f0db5acea7f2965309ea98296822a
Shahed Ahmad <shaheda.a16@gmail.com>
2021-11-15 22:13:59 +0430

    useQuery used and all the object is logged and the success error and loading is also used

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import ComponentA from './Components/ComponentA';
 import { QueryClient, QueryClientProvider } from 'react-query'
+import Posts from './Components/Posts';
 
 const queryClient = new QueryClient();
 
@@ -11,9 +11,9 @@ function App() {
     <QueryClientProvider client={queryClient}>
 
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -23,9 +23,11 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a> */}
+        </a>
         <ComponentA />
-      </header>
+      </header> */}
+      <h1>Lorem Ipsom</h1>
+      <Posts />
     </div>
     </QueryClientProvider>
   );
---
+++ src/Components/Posts.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useQuery } from 'react-query'
+
+async function fetchPosts() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10&_pages=0")
+    return response.json()
+}
+
+function Posts() {
+    const resp = useQuery("posts", fetchPosts)
+    console.log(resp)
+    const { data, isLoading, isError, error } = resp
+
+    if (isLoading) {
+        return <h3>Loading...</h3>
+    }
+
+    if (isError) return <><h4>Something went wrong</h4><h4>{error.toString()}</h4></>
+
+    return (
+        <div>
+            <ul>
+                {data && data.map(post => (
+                    <li key={post.id}>
+                        {post.title}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default Posts
